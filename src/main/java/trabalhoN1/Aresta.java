24ceea78de6222625336d5ca2395d77f06cc5c8e
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2024-09-23 20:07:56 -0300

    atualização aresta

--- src/main/java/trabalhoN1/Aresta.java
+++ src/main/java/trabalhoN1/Aresta.java
@@ -20,39 +20,48 @@ public class Aresta{
 
     //métodos get e set 
     public int getOrigem() {
+
         return origem;
     }
 
     public void setOrigem(int origem) {
+
         this.origem = origem;
     }
 
     public int getDestino() {
+
         return destino;
     }
 
     public void setDestino(int destino) {
+
         this.destino = destino;
     }
 
     public int getDistancia() {
+
         return distancia;
     }
 
     public void setDistancia(int distancia) {
+
         this.distancia = distancia;
     }
 
     public Map<String, Integer> getTransporte() {
+
         return transporte;
     }
 
     public void setTransporte(Map<String, Integer> transporte) {
+
         this.transporte = transporte;
     }
 
     //adionar um meio de transporte e o preço
     public void adicionarTransporte(String tipo, int preco){
+
         transporte.put(tipo, preco);
     }
 }
